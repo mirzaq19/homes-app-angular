86efda8e81a717bad9919f55ccd0c36849797a9b
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-01-31 13:50:21 +0700

    feat: add route to home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,14 @@ import { RouterModule } from "@angular/router";
   imports: [HomeComponent, RouterModule],
   template: `<main>
     <header class="brand-name">
-      <img
-        class="brand-logo"
-        src="assets/logo.svg"
-        alt="logo"
-        aria-hidden="true"
-      />
+      <a routerLink="/">
+        <img
+          class="brand-logo"
+          src="assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </a>
     </header>
     <section class="content">
       <router-outlet></router-outlet>
