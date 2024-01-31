71446b86075cfe3e148e5178477c6fd74ccc8534
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-01-31 12:21:47 +0700

    refactor: get housing location data from service

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,19 +1,24 @@
 import { Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ActivatedRoute } from "@angular/router";
+import { HousingLocation } from "../housing-location";
+import { HousingService } from "../housing.service";
 
 @Component({
   selector: "app-details",
   standalone: true,
   imports: [CommonModule],
-  template: ` <p>details works! {{ housingLocationId }}</p> `,
+  template: ` <p>details works! {{ housingLocation?.id }}</p> `,
   styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  housingService: HousingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params["id"]);
+    const housingLocationId = Number(this.route.snapshot.params["id"]);
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
   }
 }
